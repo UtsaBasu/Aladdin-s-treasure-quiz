1f891823cb799010a38517c9acb5566b7f3b17a0
UtsaBasu <76468540+UtsaBasu@users.noreply.github.com>
2021-06-02 12:39:38 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "LAGO";
+const accessCode2 = "VIZIER";
+const accessCode3 = "ABABWA";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("G O A L", 100,50)
+    fill("lightblue")
+    text("Hint: What was the name of Jafar's parrot ?", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+text("W B A A A B", 100,300)
+text("Hint: From which place did Prince Ali come", 100,330)
+
+text("E I V R Z I ", 700,50)
+text("Hint: What is Jafar's royal title? ", 700,70)
+}
